bf81a566fecea9b42a49c7ae52f695c73f143c30
Kenny <69609195+KennyDxsale@users.noreply.github.com>
2022-05-03 15:51:34 -0400

    updated takedev function

--- Burn_Token/burn_token.sol
+++ Burn_Token/burn_token.sol
@@ -895,7 +895,7 @@ contract BurnFeeToken is Context, IERC20, Ownable {
         uint256 rDev = tDev.mul(currentRate);
         _rOwned[_devWalletAddress] = _rOwned[_devWalletAddress].add(rDev);
         if(_isExcluded[_devWalletAddress])
-            _tOwned[_devWalletAddress] = _tOwned[_devWalletAddress].add(rDev);
+            _tOwned[_devWalletAddress] = _tOwned[_devWalletAddress].add(tDev);
     }    
     
     function calculateTaxFee(uint256 _amount) private view returns (uint256) {
