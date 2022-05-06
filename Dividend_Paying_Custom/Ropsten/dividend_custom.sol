0f5d178faa6c33a3906a95b3ba151eb81c37c3cd
Kenny <69609195+KennyDxsale@users.noreply.github.com>
2022-05-06 13:47:18 -0400

    updated verifiable dividend token

--- Dividend_Paying_Custom/Ropsten/dividend_custom.sol
+++ Dividend_Paying_Custom/Ropsten/dividend_custom.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.7;
 
 interface IERC20 {
 
@@ -202,18 +202,7 @@ interface IUniswapV2Router01 {
     function factory() external pure returns (address);
 
     function WETH() external pure returns (address);
-/*
-    function addLiquidity(
-        address tokenA,
-        address tokenB,
-        uint amountADesired,
-        uint amountBDesired,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB, uint liquidity);
-*/
+
     function addLiquidityETH(
         address token,
         uint amountTokenDesired,
@@ -223,111 +212,9 @@ interface IUniswapV2Router01 {
         uint deadline
     ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
 
-    /*function removeLiquidity(
-        address tokenA,
-        address tokenB,
-        uint liquidity,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB);
-    function removeLiquidityETH(
-        address token,
-        uint liquidity,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountToken, uint amountETH);
-    function removeLiquidityWithPermit(
-        address tokenA,
-        address tokenB,
-        uint liquidity,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline,
-        bool approveMax, uint8 v, bytes32 r, bytes32 s
-    ) external returns (uint amountA, uint amountB);
-    function removeLiquidityETHWithPermit(
-        address token,
-        uint liquidity,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline,
-        bool approveMax, uint8 v, bytes32 r, bytes32 s
-    ) external returns (uint amountToken, uint amountETH);*/
-/*
-    function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
-*/
- /*   function swapTokensForExactTokens(
-        uint amountOut,
-        uint amountInMax,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
-*/
-
-/*
-    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
-    external
-    payable
-    returns (uint[] memory amounts);
-*/
-/*
-    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
-    external
-    returns (uint[] memory amounts);
-
-
-    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
-    external
-    returns (uint[] memory amounts);
-
-    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
-    external
-    payable
-    returns (uint[] memory amounts);
-
-    function quote(uint amountA, uint reserveA, uint reserveB) external pure returns (uint amountB);
-
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external pure returns (uint amountOut);
-
-    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external pure returns (uint amountIn);
-
-    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
-
-    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
-    */
 }
 
 interface IUniswapV2Router02 is IUniswapV2Router01 {
-    /*function removeLiquidityETHSupportingFeeOnTransferTokens(
-        address token,
-        uint liquidity,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountETH);
-    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
-        address token,
-        uint liquidity,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline,
-        bool approveMax, uint8 v, bytes32 r, bytes32 s
-    ) external returns (uint amountETH);*/
 
     function swapExactTokensForTokensSupportingFeeOnTransferTokens(
         uint amountIn,
@@ -355,62 +242,11 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
 }
 
 interface IUniswapV2Factory {
-   // event PairCreated(address indexed token0, address indexed token1, address pair, uint);
-
-    /*function feeTo() external view returns (address);
-    function feeToSetter() external view returns (address);
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-    function allPairs(uint) external view returns (address pair);
-    function allPairsLength() external view returns (uint);*/
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
 
-    /*function setFeeTo(address) external;
-    function setFeeToSetter(address) external;*/
 }
 
-/*interface IUniswapV2Pair {
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
-    function name() external pure returns (string memory);
-    function symbol() external pure returns (string memory);
-    function decimals() external pure returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
-    function allowance(address owner, address spender) external view returns (uint);
-    function approve(address spender, uint value) external returns (bool);
-    function transfer(address to, uint value) external returns (bool);
-    function transferFrom(address from, address to, uint value) external returns (bool);
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint);
-    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
-    event Swap(
-        address indexed sender,
-        uint amount0In,
-        uint amount1In,
-        uint amount0Out,
-        uint amount1Out,
-        address indexed to
-    );
-    event Sync(uint112 reserve0, uint112 reserve1);
-    function MINIMUM_LIQUIDITY() external pure returns (uint);
-    function factory() external view returns (address);
-    function token0() external view returns (address);
-    function token1() external view returns (address);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function price0CumulativeLast() external view returns (uint);
-    function price1CumulativeLast() external view returns (uint);
-    function kLast() external view returns (uint);
-    function mint(address to) external returns (uint liquidity);
-    function burn(address to) external returns (uint amount0, uint amount1);
-    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
-    function skim(address to) external;
-    function sync() external;
-    function initialize(address, address) external;
-}*/
 
 library IterableMapping {
     struct Map {
@@ -530,25 +366,6 @@ interface IDividendPayingToken {
 }
 
 library SafeMathInt {
-    /*function mul(int256 a, int256 b) internal pure returns (int256) {
-        require(!(a == - 2 ** 255 && b == - 1) && !(b == - 2 ** 255 && a == - 1));
-        int256 c = a * b;
-        require((b == 0) || (c / b == a));
-        return c;
-    }
-    function div(int256 a, int256 b) internal pure returns (int256) {
-        require(!(a == - 2 ** 255 && b == - 1) && (b > 0));
-        return a / b;
-    }
-    function sub(int256 a, int256 b) internal pure returns (int256) {
-        require((b >= 0 && a - b <= a) || (b < 0 && a - b > a));
-        return a - b;
-    }
-    function add(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a + b;
-        require((b >= 0 && c >= a) || (b < 0 && c < a));
-        return c;
-    }*/
 
     function toUint256Safe(int256 a) internal pure returns (uint256) {
         require(a >= 0);
@@ -679,15 +496,15 @@ contract DividendPayingToken is ERC20, IDividendPayingToken, IDividendPayingToke
 
 contract DividendGeneratorToken is ERC20, Ownable {
 
-    IUniswapV2Router02 public uniswapV2Router;
+    IUniswapV2Router02 public immutable uniswapV2Router;
     address public immutable uniswapV2Pair;
 
     address public rewardToken;
     uint8 public decimalNew;
+    address public basePair;
 
     bool public mintedByDxsale = true;
     address dead = 0x000000000000000000000000000000000000dEaD;
-
 
     bool private swapping;
 
@@ -725,18 +542,18 @@ contract DividendGeneratorToken is ERC20, Ownable {
     );
 
     function setLiquidityFee(uint256 _newFee) external onlyOwner {
-        require(_newFee > 0 && _newFee <= 10, "Fee out of range!");
+        require(_newFee >= 0 && _newFee <= 10, "Fee out of range!");
         liquidityFee = _newFee;
         totalFees = _newFee + tokenRewardsFee;
     }
 
     function setRewardFee(uint256 _newFee) external onlyOwner {
-        require(_newFee > 0 && _newFee <= 15, "Fee out of range!");
+        require(_newFee >= 0 && _newFee <= 15, "Fee out of range!");
         tokenRewardsFee = _newFee;
         totalFees = _newFee + liquidityFee;
     }
 
-    constructor(address _rewardToken, string memory name_, string memory symbol_, uint8 decimals_, uint256 _totalSupply, uint8 _tokenRewardsFee, uint8 _liquidityFee, address _router) ERC20(name_, symbol_) {
+    constructor(address _rewardToken, string memory name_, string memory symbol_, uint8 decimals_, uint256 _totalSupply, uint8 _tokenRewardsFee, uint8 _liquidityFee, address _router, address _basePair) ERC20(name_, symbol_) {
 
         tokenRewardsFee = _tokenRewardsFee;
         liquidityFee = _liquidityFee;
@@ -745,19 +562,19 @@ contract DividendGeneratorToken is ERC20, Ownable {
         rewardToken = _rewardToken;
         decimalNew = decimals_;
 
-        swapTokensAtAmount = (_totalSupply * (10 ** decimalNew)) /10000;
-        minimumTokenBalanceForDividends = (_totalSupply * (10 ** decimalNew))  /100000;
+        swapTokensAtAmount = _totalSupply /10000;
+        minimumTokenBalanceForDividends = _totalSupply /100000;
 
         dividendTracker = new DividendTracker(rewardToken, minimumTokenBalanceForDividends, decimalNew);
 
+        basePair = _basePair;  
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(_router);
 
         address _uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
-        .createPair(address(this), _uniswapV2Router.WETH());
+            .createPair(address(this), basePair);
 
         uniswapV2Router = _uniswapV2Router;
         uniswapV2Pair = _uniswapV2Pair;
-
 
         // exclude from receiving dividends
         dividendTracker.excludeFromDividends(address(dividendTracker));
@@ -769,7 +586,7 @@ contract DividendGeneratorToken is ERC20, Ownable {
         //excludeFromFees(address(this), true);
         _isExcludedFromFees[address(this)] = true;
 
-        _mint(tx.origin, _totalSupply * (10 ** decimalNew));
+        _mint(tx.origin, _totalSupply);
 
     }
 
@@ -777,28 +594,15 @@ contract DividendGeneratorToken is ERC20, Ownable {
 
     }
 
+    function getWrapAddr() public view returns (address){
+
+        return basePair;
+
+    }
+
     function decimals() public view virtual override returns (uint8) {
         return decimalNew;
     }
-
-    /*function whitelistDxSale(address _presaleAddress, address _routerAddress) public onlyOwner {
-        presaleAddress = _presaleAddress;
-
-        dividendTracker.excludeFromDividends(_presaleAddress);
-        excludeFromFees(_presaleAddress, true);
-        dividendTracker.excludeFromDividends(_routerAddress);
-        excludeFromFees(_routerAddress, true);
-    }
-
-    function updateUniswapV2Router(address newAddress) public onlyOwner {
-        require(newAddress != address(uniswapV2Router), "The router already has that address");
-        uniswapV2Router = IUniswapV2Router02(newAddress);
-    }
-
-    function excludeFromFees(address account, bool excluded) public onlyOwner {
-        require(_isExcludedFromFees[account] != excluded, "Account is already the value of 'excluded'");
-        _isExcludedFromFees[account] = excluded;
-    }*/
 
     function excludeFromFees(address account) public onlyOwner {
         require(!_isExcludedFromFees[account], "Account is already excluded");
@@ -948,7 +752,7 @@ contract DividendGeneratorToken is ERC20, Ownable {
     function swapTokensForEth(uint256 tokenAmount) private {
         address[] memory path = new address[](2);
         path[0] = address(this);
-        path[1] = uniswapV2Router.WETH();
+        path[1] = getWrapAddr();
         _approve(address(this), address(uniswapV2Router), tokenAmount);
         uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
             tokenAmount,
@@ -963,7 +767,7 @@ contract DividendGeneratorToken is ERC20, Ownable {
     function swapTokensForReward(uint256 tokenAmount, address recipient) private {
         address[] memory path = new address[](3);
         path[0] = address(this);
-        path[1] = uniswapV2Router.WETH();
+        path[1] = getWrapAddr();
         path[2] = rewardToken;
 
         _approve(address(this), address(uniswapV2Router), tokenAmount);
